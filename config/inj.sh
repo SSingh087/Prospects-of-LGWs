1ecd509232dad49f5bdf259e06bb8898a5aaecbc
SSingh087 <shashwat98singh@gmail.com>
2022-05-19 20:47:20 +0200

    small tests

---
+++ config/inj.sh
@@ -0,0 +1,6 @@
+pycbc_create_injections --verbose \
+			--ninjections 100 \
+			--config-files inj.ini \
+			--output-file injection.hdf \
+			--force \
+			--seed 10
---
+++ config/run.sh
@@ -0,0 +1,5 @@
+/home/ss/Prospects-of-LGWs/config/evaluate_SNR \
+--inference-config /home/ss/Prospects-of-LGWs/config/prior.ini \
+--injection-file /home/ss/Prospects-of-LGWs/config/injection.hdf \
+--instruments E1 E2 E3 \
+
