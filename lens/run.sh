4a956e4e9fac66d9c4ed4bc193152b52a7823a34
SSingh087 <shashwat98singh@gmail.com>
2022-05-25 11:54:20 +0200

    lens and unlens dir

---
+++ lens/run.sh
@@ -0,0 +1,8 @@
+cp /home/ss/Prospects-of-LGWs/unlens/injection.hdf ./ --verbose
+
+/home/ss/Prospects-of-LGWs/lens/evaluate_SNR \
+--injection-file-lens-dir /home/ss/Prospects-of-LGWs/lens/injection \
+--injection-file-unlens-dir /home/ss/Prospects-of-LGWs/unlens/injection \
+--injection-file /home/ss/Prospects-of-LGWs/lens/injection.hdf \
+--instruments E1 E2 E3 \
+
