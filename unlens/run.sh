aad1191ae5d77b93bc84056b7196a75ee6bffdc3
SSingh087 <shashwat98singh@gmail.com>
2022-05-25 11:55:39 +0200

    changes

---
+++ unlens/run.sh
@@ -0,0 +1,5 @@
+/home/ss/Prospects-of-LGWs/unlens/evaluate_SNR \
+--inference-config /home/ss/Prospects-of-LGWs/unlens/prior.ini \
+--injection-file /home/ss/Prospects-of-LGWs/unlens/injection.hdf \
+--instruments E1 E2 E3 \
+
