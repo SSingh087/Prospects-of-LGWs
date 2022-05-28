654716ac9e2d2251aba0e66320d19d41ad6adda4
SSingh087 <shashwat98singh@gmail.com>
2022-05-28 20:02:02 +0200

    outline ready

--- lens/run.sh
+++ lens/run.sh
@@ -1,9 +1,9 @@
 #cp /home/ss/Prospects-of-LGWs/unlens/injection.hdf ./ --verbose
 
-/content/Prospects-of-LGWs/lens/evaluate_SNR \
---injection-file-lens-dir /content/Prospects-of-LGWs/lens/injection \
---injection-file-unlens-dir /content/Prospects-of-LGWs/unlens/injection \
---injection-file /content/Prospects-of-LGWs/lens/injection.hdf \
---inference-config /content/Prospects-of-LGWs/lens/prior.ini \
+/home/ss/Prospects-of-LGWs/lens/evaluate_SNR \
+--injection-file-lens-dir /home/ss/Prospects-of-LGWs/lens/injection \
+--injection-file-unlens-dir /home/ss/Prospects-of-LGWs/unlens/injection \
+--injection-file /home/ss/Prospects-of-LGWs/lens/injection.hdf \
+--inference-config /home/ss/Prospects-of-LGWs/lens/prior.ini \
 --instruments E1 E2 E3 \
 
