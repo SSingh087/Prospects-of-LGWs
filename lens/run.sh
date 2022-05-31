e3d90f41694c595fdc18631718709cb2b2154c53
SSingh087 <shashwat98singh@gmail.com>
2022-05-31 19:35:26 +0000

    some figs

---
+++ lens/run.sh
@@ -0,0 +1,31 @@
+
+/work/shashwat.singh/Prospects-of-LGWs/lens/evaluate_SNR \
+--inference-config /work/shashwat.singh/Prospects-of-LGWs/unlens/prior.ini \
+--injection-file /work/shashwat.singh/Prospects-of-LGWs/unlens/injection.hdf \
+--output-dir /work/shashwat.singh/Prospects-of-LGWs/lens/ET \
+--instruments E1 E2 E3 \
+
+/work/shashwat.singh/Prospects-of-LGWs/lens/evaluate_SNR \
+--inference-config /work/shashwat.singh/Prospects-of-LGWs/unlens/prior.ini \
+--injection-file /work/shashwat.singh/Prospects-of-LGWs/unlens/injection.hdf \
+--output-dir /work/shashwat.singh/Prospects-of-LGWs/lens/CE \
+--instruments C1 C2 \
+
+/work/shashwat.singh/Prospects-of-LGWs/lens/evaluate_SNR \
+--inference-config /work/shashwat.singh/Prospects-of-LGWs/unlens/prior.ini \
+--injection-file /work/shashwat.singh/Prospects-of-LGWs/unlens/injection.hdf \
+--output-dir /work/shashwat.singh/Prospects-of-LGWs/lens/ET_CE \
+--instruments E1 E2 E3 C1 C2 \
+
+/work/shashwat.singh/Prospects-of-LGWs/lens/evaluate_SNR \
+--inference-config /work/shashwat.singh/Prospects-of-LGWs/unlens/prior.ini \
+--injection-file /work/shashwat.singh/Prospects-of-LGWs/unlens/injection.hdf \
+--output-dir /work/shashwat.singh/Prospects-of-LGWs/lens/ALL \
+--instruments E1 E2 E3 C1 C2 H1 V1 L1 I1 K1 \
+
+/work/shashwat.singh/Prospects-of-LGWs/lens/evaluate_SNR \
+--inference-config /work/shashwat.singh/Prospects-of-LGWs/unlens/prior.ini \
+--injection-file /work/shashwat.singh/Prospects-of-LGWs/unlens/injection.hdf \
+--output-dir /work/shashwat.singh/Prospects-of-LGWs/lens/HLVKI \
+--instruments H1 V1 L1 I1 K1 \
+
