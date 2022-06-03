d2888a7bc800d8a265733a2447bc2f4efc94d264
SSingh087 <shashwat98singh@gmail.com>
2022-06-03 17:23:33 +0000

    sim complete 100

--- lens/run.sh
+++ lens/run.sh
@@ -1,5 +1,30 @@
 /work/shashwat.singh/Prospects-of-LGWs/lens/evaluate_SNR \
+--injection-file /work/shashwat.singh/Prospects-of-LGWs/unlens/injection_100.hdf \
 --inference-config /work/shashwat.singh/Prospects-of-LGWs/unlens/prior.ini \
---injection-file /work/shashwat.singh/Prospects-of-LGWs/unlens/injection.hdf \
+--output-dir /work/shashwat.singh/Prospects-of-LGWs/lens/CE \
+--instruments C1 C2\
+
+/work/shashwat.singh/Prospects-of-LGWs/lens/evaluate_SNR \
+--injection-file /work/shashwat.singh/Prospects-of-LGWs/unlens/injection_100.hdf \
+--inference-config /work/shashwat.singh/Prospects-of-LGWs/unlens/prior.ini \
 --output-dir /work/shashwat.singh/Prospects-of-LGWs/lens/ET \
---instruments E1 E2 E3 \
+--instruments E1 E2 E3\
+
+/work/shashwat.singh/Prospects-of-LGWs/lens/evaluate_SNR \
+--injection-file /work/shashwat.singh/Prospects-of-LGWs/unlens/injection_100.hdf \
+--inference-config /work/shashwat.singh/Prospects-of-LGWs/unlens/prior.ini \
+--output-dir /work/shashwat.singh/Prospects-of-LGWs/lens/ET_CE \
+--instruments E1 E2 E3 C1 C2
+
+
+/work/shashwat.singh/Prospects-of-LGWs/lens/evaluate_SNR \
+--injection-file /work/shashwat.singh/Prospects-of-LGWs/unlens/injection_100.hdf \
+--inference-config /work/shashwat.singh/Prospects-of-LGWs/unlens/prior.ini \
+--output-dir /work/shashwat.singh/Prospects-of-LGWs/lens/ALL \
+--instruments E1 E2 E3 C1 C2 H1 V1 L1 I1 K1 \
+
+/work/shashwat.singh/Prospects-of-LGWs/lens/evaluate_SNR \
+--injection-file /work/shashwat.singh/Prospects-of-LGWs/unlens/injection_100.hdf \
+--inference-config /work/shashwat.singh/Prospects-of-LGWs/unlens/prior.ini \
+--output-dir /work/shashwat.singh/Prospects-of-LGWs/lens/HLVKI \
+--instruments H1 V1 L1 I1 K1 \
---
+++ unlens/run_100.sh
@@ -0,0 +1,30 @@
+/work/shashwat.singh/Prospects-of-LGWs/unlens/evaluate_SNR \
+--injection-file /work/shashwat.singh/Prospects-of-LGWs/unlens/injection_100.hdf \
+--inference-config /work/shashwat.singh/Prospects-of-LGWs/unlens/prior.ini \
+--output-dir /work/shashwat.singh/Prospects-of-LGWs/unlens/CE \
+--instruments C1 C2\
+
+/work/shashwat.singh/Prospects-of-LGWs/unlens/evaluate_SNR \
+--injection-file /work/shashwat.singh/Prospects-of-LGWs/unlens/injection_100.hdf \
+--inference-config /work/shashwat.singh/Prospects-of-LGWs/unlens/prior.ini \
+--output-dir /work/shashwat.singh/Prospects-of-LGWs/unlens/ET \
+--instruments E1 E2 E3\
+
+/work/shashwat.singh/Prospects-of-LGWs/unlens/evaluate_SNR \
+--injection-file /work/shashwat.singh/Prospects-of-LGWs/unlens/injection_100.hdf \
+--inference-config /work/shashwat.singh/Prospects-of-LGWs/unlens/prior.ini \
+--output-dir /work/shashwat.singh/Prospects-of-LGWs/unlens/ET_CE \
+--instruments E1 E2 E3 C1 C2
+
+
+/work/shashwat.singh/Prospects-of-LGWs/unlens/evaluate_SNR \
+--injection-file /work/shashwat.singh/Prospects-of-LGWs/unlens/injection_100.hdf \
+--inference-config /work/shashwat.singh/Prospects-of-LGWs/unlens/prior.ini \
+--output-dir /work/shashwat.singh/Prospects-of-LGWs/unlens/ALL \
+--instruments E1 E2 E3 C1 C2 H1 V1 L1 I1 K1 \
+
+/work/shashwat.singh/Prospects-of-LGWs/unlens/evaluate_SNR \
+--injection-file /work/shashwat.singh/Prospects-of-LGWs/unlens/injection_100.hdf \
+--inference-config /work/shashwat.singh/Prospects-of-LGWs/unlens/prior.ini \
+--output-dir /work/shashwat.singh/Prospects-of-LGWs/unlens/HLVKI \
+--instruments H1 V1 L1 I1 K1 \
